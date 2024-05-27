77f159f2ded92072572b0ef708bfe073db16886c
Anand Pap <anandpap@live.com>
2024-05-27 07:10:14 +0200

    typo fix

--- addDownloadToButton.js
+++ addDownloadToButton.js
@@ -2,7 +2,7 @@ async function downloadCsvFileOnClick() {
   const movieIDs = await fetchMovieIDs(1000);
   const movies = await fetchMovieDetails(movieIDs);
   const csvData = formCsvData(movies);
-  downloadCsvFile(csvData, "top_rate_movies.csv");
+  downloadCsvFile(csvData, "top_rated_movies.csv");
 }
 
 document.getElementById("download-csv-button").addEventListener("click", downloadCsvFileOnClick);
